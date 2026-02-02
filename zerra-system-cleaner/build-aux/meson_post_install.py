216bab0ae7c6bf017ae98f848e8b5d8479730c6e
Mustafa Gökpınar <mustafagokpinar4646@gmail.com>
2026-02-02 22:13:33 +0300

    build: Switch to Meson build system and update Flatpak manifest with refined permissions.

---
+++ zerra-system-cleaner/build-aux/meson_post_install.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+import os
+import subprocess
+
+install_prefix = os.environ.get('MESON_INSTALL_PREFIX', '/usr/local')
+datadir = os.path.join(install_prefix, 'share')
+
+if not os.environ.get('DESTDIR'):
+    print('Updating icon cache...')
+    subprocess.call(['gtk-update-icon-cache', '-qtf', os.path.join(datadir, 'icons', 'hicolor')])
+
+    print('Updating desktop database...')
+    subprocess.call(['update-desktop-database', '-q', os.path.join(datadir, 'applications')])
+
+    print('Updating mime database...')
+    subprocess.call(['update-mime-database', os.path.join(datadir, 'mime')])
